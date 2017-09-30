7eb9179509e04c2133c5d46753ba64bc1f980e59
Kenza <kenza.dieye@gmail.com>
2017-09-30 17:25:04 +0200

    Embedded adding review done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -17,6 +17,7 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+    @review = Review.new
     @restaurant = Restaurant.find(params[:id])
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :restaurants, only: [:create, :index, :new, :show]
   resources :restaurants do
-    resources :reviews, only: [ :index, :new, :create ]
+    resources :reviews, only: [ :index, :create ]
   end
   resources :reviews, only: [ :show ]
+  post "restaurants/:id/reviews", to: 'reviews#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "controller#action"
 end
