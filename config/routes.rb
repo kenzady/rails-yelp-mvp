23cefe9aba4f4f8be61d791e7a42ff154693bd7f
Kenza <kenza.dieye@gmail.com>
2017-09-30 15:00:56 +0200

    restaurant controller setted

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :restaurants, only: [:create, :index, :new, :show]
+  resources :restaurants do
+    resources :reviews, only: [ :index, :new, :create ]
+  end
+  resources :reviews, only: [ :show ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "controller#action"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category:     "french",
+
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    category:     "french",
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:     "italian",
+  },
+  {
+    name:         "La Friterie",
+    address:      "route de Suresnes 75016 Paris",
+    category:     "belgian",
+  },
+  {
+    name:         "Ban Bao",
+    address:      "route de Franz 75011 Paris",
+    category:     "japanese",
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
