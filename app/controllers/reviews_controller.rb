8f83212779daaad14935f4d3e498f38cab8a1c50
Kenza <kenza.dieye@gmail.com>
2017-09-30 16:09:56 +0200

    Review controller setted

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,12 +7,21 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
+    # @review.rating = 5
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @reviews = Review.all.restaurant_id
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
